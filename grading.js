03a267e88d0ebe3d21da1d412b2e6d0cb9a98055
Avelin-cmd <avelinlohay@gmail.com>
2026-07-07 15:57:53 +0300

    second commit

---
+++ grading.js
@@ -0,0 +1,18 @@
+let nane = prompt ("enter the student's name")
+let marks = prompt ("enter your marks")
+if(marks >=80){
+    alert("A")
+}
+else if(marks >=70){
+    alert("B")
+}
+else if(marks >=60){
+    alert("C")
+}
+else if (marks >=50){
+    alert("D")
+}
+else{
+    alert("F")
+}
+alert(name + marks)
